efc9a4ef1b58e3c5c58012a52eeaca53496bbafe
Jo Wagner <jowagner@limago.de>
2022-04-02 17:40:54 +0200

    menu added

---
+++ seminar-manager/trainerdatenbank/src/frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- img src="./assets/logo.png"-->
+    <NavMenu></NavMenu>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import NavMenu from './components/NavMenu'
+export default {
+  name: 'App',
+  components: {NavMenu},
+  mounted() {
+    document.title = "Trainerdatenbank";
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ seminar-manager/trainerdatenbank/src/main/java/de/limago/trainerdatenbank/TrainerdatenbankApplication.java
@@ -0,0 +1,13 @@
+package de.limago.trainerdatenbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerdatenbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainerdatenbankApplication.class, args);
+    }
+
+}
